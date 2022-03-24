55494a8f675c910321fd51195ffe24f75ae7f9eb
Ben Manley <benjamintmanley@gmail.com>
2022-03-23 19:06:49 -0500

    add service functions for get profile and add villager

---
+++ src/services/profileService.js
@@ -0,0 +1,31 @@
+import * as tokenService from '../services/tokenService'
+
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/profiles`
+
+async function getAllProfiles() {
+  const res = await fetch(BASE_URL, {
+    headers: { Authorization: `Bearer ${tokenService.getToken()}` },
+  })
+  return await res.json()
+}
+
+function getProfile(id) {
+  return fetch(`${BASE_URL}/${id}`, {
+    headers: { Authorization: `Bearer ${tokenService.getToken()}` },
+  })
+  .then(res => res.json())
+}
+
+function addVillager(villager) {
+  return fetch(`${BASE_URL}/addVillager`, {
+    method: "POST",
+    headers: { 
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(villager)
+  })
+  .then(res => res.json())
+}
+
+export { getAllProfiles }
